8c3f2f00944b347aad668b1c658e119a0bfb9c78
Suresh-medimudi <jagadish.medimudi@sasi.ac.in>
2024-11-07 12:49:22 +0530

    Add files via upload

---
+++ Stringdemo.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class Stringdemo{
+public static void main(String args[]){
+String s1="welcome";
+char[]ch={'S','U','R','E','S','H'};
+String s2=new String(ch);
+String s3=new String("Example");
+System.out.println("the string s1 is: " + s1);
+System.out.println("the string s2 is: " + s2);
+System.out.println("the string s3 is: " + s3);
+}
+}
